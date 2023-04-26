6653be2d6a745cd500f9fefb311263d609c1d241
LeaSimonet <simonetlea97@gmail.com>
2023-04-26 20:02:55 +0200

    Ajout de la correction app

--- Working space/JPA/maven2/src/main/java/Jpa/context/Jpacontext.java
+++ Working space/JPA/maven2/src/main/java/Jpa/context/Jpacontext.java
@@ -3,9 +3,20 @@ package Jpa.context;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import formation.Jpa.repositories.DaoProduit;
+import formation.Jpa.repositories.DaoProduitJpaImpl;
+
 public class Jpacontext {
 	
 	private static EntityManagerFactory entityManagerFactory;
+	private static DaoProduit daoProduit=new DaoProduitJpaImpl();
+	
+	
+	
+	public static DaoProduit getDaoProduit() {
+		return daoProduit;
+	}
+	
 	public static  EntityManagerFactory getEntityManagerFactory () {
 		if(entityManagerFactory==null) {
 			entityManagerFactory=Persistence.createEntityManagerFactory("eshop");
--- Working space/JPA/maven2/src/test/java/formation/jpa/AppTest.java
+++ Working space/JPA/maven2/src/test/java/formation/jpa/AppTest.java
@@ -5,11 +5,13 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import Jpa.context.Jpacontext;
 import formation.Jpa.entities.Produit;
+import formation.Jpa.repositories.DaoProduit;
 
 public class AppTest {
 	public static void main(String[]args) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("eshop");
+		/*EntityManagerFactory emf = Persistence.createEntityManagerFactory("eshop");
 		System.out.println(emf);
 		
 		
@@ -36,14 +38,14 @@ public class AppTest {
 		Produit produitEnBase=em.find(Produit.class, 1L);
 		System.out.println(produitEnBase);
 		
-		/*Ces deux lignes de code sont écrites en langage Java et utilisent la bibliothèque Java Persistence API (JPA) pour interagir avec une base de données relationnelle.
+		Ces deux lignes de code sont écrites en langage Java et utilisent la bibliothèque Java Persistence API (JPA) pour interagir avec une base de données relationnelle.
 		La première ligne de code exécute une requête de recherche dans la base de données pour récupérer l'objet "Produit" 
 		correspondant à la clé primaire "1L" (qui est de type long). Cette requête utilise la méthode "find" fournie par l'objet "EntityManager" 
 		(abrévié "em" dans le code) qui représente le contexte de persistance de JPA. La variable "produitEnBase" est une référence à l'objet "Produit" trouvé dans la base de données.
 		La deuxième ligne de code affiche le contenu de la variable "produitEnBase" à la console en utilisant la méthode "println" fournie par la classe "System". 
 		Cela permet de vérifier que l'objet "Produit" a bien été récupéré depuis la base de données et que son contenu est correct. Le résultat affiché dépend de l'implémentation de la méthode 
 		"toString" de la classe "Produit".
-		*/
+		
 		
 		em.close();
 		
@@ -59,18 +61,38 @@ public class AppTest {
 		tx.commit();
 		em.close();
 		
-		///////DELETE
-		/*em=emf.createEntityManager();
+		DELETE
+		em=emf.createEntityManager();
 		tx=em.getTransaction();
 		tx.begin();
 		em.remove(em.merge(produitEnBase));
 		tx.commit();
-		em.close();*/
+		em.close();
 		// on peut garantir que l'objet est dans la base avec un remove. Il faut donc faire un merge de notre produit. La véritable opération de merge est de rattacher l'objet en base. 
 		// Je vais être obliger de prendre le produit de le rattacher à la base et ensuite de remove 
 		
 		
 		emf.close();
+		*/
+		
+		Produit p1 = new Produit();
+		p1.setId(1L);
+		p1.setNom("TV");
+		p1.setPrix(100);
+		
+		DaoProduit daoProduit=Jpacontext.getDaoProduit();
+		daoProduit.insert(p1);
+		p1.setDescription("Cette TV est super cool");
+		daoProduit.update(p1);
+		System.out.println(daoProduit.findAll());
+		
+		// EN dernier
+		
+		Jpacontext.destroy();
+		
+		
+		
+		
 	}
 	
 
